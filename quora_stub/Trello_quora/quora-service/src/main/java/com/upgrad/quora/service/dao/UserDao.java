5f834c3553e5d34fe04b337113e3344dbd0c10bc
shreya <shreya@zyllu.com>
2020-07-11 15:09:17 +0530

    Added functionality endpoint for sign up of user

---
+++ quora_stub/Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,41 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity createUser(UserEntity userEntity) {
+        entityManager.persist(userEntity);
+        return userEntity;
+    }
+
+    public boolean isEmailExists(String email) {
+        try {
+            entityManager.createNamedQuery("userByEmail", UserEntity.class)
+                    .setParameter("email", email).getSingleResult();
+            return true;
+        }catch(NoResultException nre) {
+            return false;
+        }
+    }
+
+    public boolean isUsernameExists(String username) {
+        try {
+            entityManager.createNamedQuery("userByUsername", UserEntity.class)
+                    .setParameter("username", username).getSingleResult();
+            return true;
+        }catch(NoResultException nre) {
+            return false;
+        }
+    }
+
+}
