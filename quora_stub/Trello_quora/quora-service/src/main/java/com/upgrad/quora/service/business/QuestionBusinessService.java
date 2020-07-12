df16741b922428cf30767cea9a738b9f41610f81
shreya <shreya@zyllu.com>
2020-07-12 18:30:43 +0530

    Committing the functionality endpoint of createQuestion for creating question by a user

---
+++ quora_stub/Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -0,0 +1,34 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class QuestionBusinessService {
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String authorization) throws AuthorizationFailedException{
+        UserAuthTokenEntity userAuthTokenEntity = userDao.getUserAuthToken(authorization);
+        if(userAuthTokenEntity==null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+        if(userAuthTokenEntity.getLogout_at()!=null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post a question");
+        }
+        questionEntity.setUser(userAuthTokenEntity.getUser());
+        return questionDao.createQuestion(questionEntity);
+    }
+
+}
